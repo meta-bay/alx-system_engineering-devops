055429c149cd6b1b1081b56b40ae126926493c69
meta-bay <bayiemeseret@gmail.com>
2024-02-25 23:48:49 +0300

    client configuration file (w/puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+file_line {:
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line {:
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
+
