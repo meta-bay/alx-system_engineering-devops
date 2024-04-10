9e053b79f977d0a0a567cfc9face1106f453fd9a
meta-bay <bayiemeseret@gmail.com>
2024-04-10 04:44:02 +0300

    reddit subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' How many subscribers'''
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''
+    Request number of subs
+    '''
+    user_agent = 'custom_user_agent'
+    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
+
+    headers = {'User-Agent': user_agent}
+
+    request = requests.get(url, headers=headers, allow_redirects=False)
+
+    if request.status_code != 200:
+        return 0
+
+    data = request.json()['data']
+    data_child = data['children']
+    info_data = data_child[0]['data']
+    return info_data['subreddit_subscribers'], "OK"
