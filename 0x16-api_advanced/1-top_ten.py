dd4193fe7776875f9fc84f6dac9f997280240847
meta-bay <bayiemeseret@gmail.com>
2024-04-10 05:30:03 +0300

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-hot posts on a subreddit
+    queries the Reddit API and prints the titles of
+    the first 10 hot posts listed for a given subreddit.
 """
 
 import requests
