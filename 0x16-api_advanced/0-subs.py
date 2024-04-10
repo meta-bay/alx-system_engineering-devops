c889b9c946f473b833cba17695f362eb613aabc9
meta-bay <bayiemeseret@gmail.com>
2024-04-10 05:05:48 +0300

    just work

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "custom user agent"}
     req = requests.get(url, headers=headers, allow_redirects=False)
-    if req.status_code != 200:
+    if req.status_code == 404:
         return 0
     data = req.json().get("data")
     return data.get("subscribers")
