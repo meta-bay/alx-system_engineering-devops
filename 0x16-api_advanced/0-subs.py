d85e3061660e5e1b44f2d61690eac26f57619d4d
meta-bay <bayiemeseret@gmail.com>
2024-04-10 06:00:11 +0300

    it doesn't work

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
         gets number of subs
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "custom agent0"}
+    headers = {"User-Agent": "custom agent 0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ import requests
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts """
-    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "custom agent 2"}
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 404:
