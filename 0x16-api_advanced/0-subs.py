1bd4f06ffb6323356ca255343a689b2d064d004f
meta-bay <bayiemeseret@gmail.com>
2024-04-10 05:05:01 +0300

    just work

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-''' How many subscribers'''
+"""How many subscribers"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    '''
+    """
         gets number of subs
-    '''
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "custom user agent"}
     req = requests.get(url, headers=headers, allow_redirects=False)
